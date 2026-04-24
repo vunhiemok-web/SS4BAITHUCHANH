8df988826337723599c4ee12cff1dcc138682160
Nguyễn Đăng Khoa <dkhoi0209@gmail.com>
2026-04-24 13:48:22 +0700

    Sua lai phan update

--- BAI THUC HANH.sql
+++ BAI THUC HANH.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE ShopManager;
+USE shopmanager;
 
 CREATE TABLE Categories (
 	category_id INT PRIMARY KEY AUTO_INCREMENT,
 	category_name VARCHAR(50) NOT NULL
-)
+);
 
 CREATE TABLE Products (
 	product_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -12,7 +12,7 @@ CREATE TABLE Products (
 	stock INT CHECK(stock > 0),
 	category_id INT NOT NULL,
     FOREIGN KEY(category_id) REFERENCES Categories(category_id)
-)
+);
 
 INSERT INTO Categories (category_name)
 VALUES ('Điện tử'),('Thời trang');
@@ -23,6 +23,7 @@ VALUES (('iPhone 15'), 25000000, 10, 1),
         (('Áo sơ mi nam'), 500000 , 50, 2),
         (('Giày thể thao'), 1200000 , 20, 2);
         
+SET SQL_SAFE_UPDATES = 0;
 UPDATE Products
 SET price = 26000000
 WHERE product_name = 'iPhone 15';
@@ -30,7 +31,7 @@ WHERE product_name = 'iPhone 15';
 UPDATE Products
 SET stock = stock + 10
 WHERE category_id = 1;
-
+SET SQL_SAFE_UPDATES = 0;
 DELETE FROM Products
 WHERE product_id = 4;
 
